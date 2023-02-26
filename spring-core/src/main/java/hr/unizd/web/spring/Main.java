f163d83558a3910ab4c632672a6a8d2cc98863b1
MBOGOVIC\mbogovic <mbogovic@croz.net>
2023-02-26 14:17:28 +0100

    Add protoype scope example

--- spring-core/src/main/java/hr/unizd/web/spring/Main.java
+++ spring-core/src/main/java/hr/unizd/web/spring/Main.java
@@ -8,10 +8,19 @@ public class Main {
         //Kreiramo aplikacijski context/spring container
         ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
-        DataReaderClient dataReaderClient = context.getBean("dataReaderClient", DataReaderClient.class);
+        DataReaderClient fileDataReaderClient = context.getBean("fileDataReaderClient", DataReaderClient.class);
+        DataReaderClient fileDataReaderClient2 = context.getBean("fileDataReaderClient2", DataReaderClient.class);
+        DataReaderClient memoryDataReaderClient = context.getBean("memoryDataReaderClient", DataReaderClient.class);
+        DataReaderClient memoryDataReaderClient2 = context.getBean("memoryDataReaderClient2", DataReaderClient.class);
 
         System.out.println("-----------------------");
-        System.out.println("Ispisujem tražene podatke : \n" + dataReaderClient.fetchData());
+        System.out.println("Ispisujem tražene podatke fileDataReaderClient: \n" + fileDataReaderClient.fetchData());
+        System.out.println("-----------------------");
+        System.out.println("Ispisujem tražene podatke fileDataReaderClient2: \n" + fileDataReaderClient2.fetchData());
+        System.out.println("-----------------------");
+        System.out.println("Ispisujem tražene podatke memoryDataReaderClient: \n" + memoryDataReaderClient.fetchData());
+        System.out.println("-----------------------");
+        System.out.println("Ispisujem tražene podatke memoryDataReaderClient2: \n" + memoryDataReaderClient2.fetchData());
         System.out.println("-----------------------");
     }
 }
